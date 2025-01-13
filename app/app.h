e8d0541a6555b7b214f9a67e9589e1aa52a6968f
Ivan Ceranac <ivcerana@vsite.hr>
2025-01-13 20:29:17 +0100

    resolved lab 06

--- app/app.h
+++ app/app.h
@@ -2,5 +2,36 @@
 
 namespace vsite::oop::v6 
 {
+	template <typename T>
+	T minimum(T a, T b) {
+		
+		if (a < b) {
+			return a;
+		}
+		else {
+			return b;
+		}
+	}
+
+	template <typename T, int Size>
+
+	class fixed_array {
+	public:
+		unsigned long long size() const {
+			return Size;
+		}
+
+		T operator [] (int i) {
+			return niz[i];
+		}
+	private:
+		T niz[Size];
+	};
+
+	template <typename T>
+	struct point {
+		T x;
+		T y;
+	};
 
 }
